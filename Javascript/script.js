b32bf6d44bc61ce72aa867f45f2b3d2a76957907
SebasAlRivas <113698098+SebasAlRivas@users.noreply.github.com>
2025-07-15 14:54:09 +0000

    btn de scroll arriba

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,8 +1,11 @@
+//accion del boton enviar
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('¡Gracias por tu mensaje, ' + document.getElementById('nombre').value + '! Te responderé pronto.');
     this.reset();
 });
+
+// aumenta los iconos de red
 document.addEventListener("DOMContentLoaded", () => {
     const iconos = document.querySelectorAll(".icono-red");
 
@@ -14,6 +17,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
         icono.addEventListener("mouseleave", () => {
             icono.style.transform = "translateY(0)";
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("btn-subir");
+
+    // Mostrar u ocultar el botón según el scroll
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            btn.style.display = "block";
+        } else {
+            btn.style.display = "none";
+        }
+    });
+
+    // Desplazar hacia arriba suavemente
+    btn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
         });
     });
 });
