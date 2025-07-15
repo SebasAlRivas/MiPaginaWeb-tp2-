b7be31ce279bbf9c4a31ce042ae4c76118c8fa48
SebasAlRivas <113698098+SebasAlRivas@users.noreply.github.com>
2025-07-15 14:14:11 +0000

    etapa 1

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,3 +3,17 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     alert('¡Gracias por tu mensaje, ' + document.getElementById('nombre').value + '! Te responderé pronto.');
     this.reset();
 });
+document.addEventListener("DOMContentLoaded", () => {
+    const iconos = document.querySelectorAll(".icono-red");
+
+    iconos.forEach(icono => {
+        icono.addEventListener("mouseenter", () => {
+            icono.style.transform = "translateY(-5px)";
+            icono.style.transition = "transform 0.3s ease";
+        });
+
+        icono.addEventListener("mouseleave", () => {
+            icono.style.transform = "translateY(0)";
+        });
+    });
+});
